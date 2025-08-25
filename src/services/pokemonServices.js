0ed428c31ad8b1d52b3bb0c86fc1d38dbaf020f1
NaghellyM <naghellymoreno0610@gmail.com>
2025-08-24 23:50:39 -0500

    feat:Pokemon API Request

--- src/services/pokemonServices.js
+++ src/services/pokemonServices.js
@@ -1,10 +1,33 @@
-import axios from "axios"
+import axios from "axios";
 
-export const getPokemonById = async ({id}) => {
+export const getPokemonById = async ({ id }) => {
     try {
-        const response = await axios.get(`${import.meta.env.VITE_URL_TO_POKEMON}/${id}`)
-        return response.data
+        const response = await axios.get(
+            `${import.meta.env.VITE_URL_TO_POKEMON}/pokemon/${id}`
+        );
+        return response.data;
     } catch (error) {
-        throw error
+        throw error;
     }
-}
+};
+export const getInfoSpecies = async ({ id }) => {
+    try {
+        const response = await axios.get(
+            `${import.meta.env.VITE_URL_TO_POKEMON}/pokemon-species/${id}`
+        );
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+};
+
+export const getPokemonMoves = async ({ id }) => {
+    try {
+        const response = await axios.get(
+            `${import.meta.env.VITE_URL_TO_POKEMON}/pokemon/moves/${id}`
+        );
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+};
